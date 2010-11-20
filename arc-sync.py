924427ccfa4553f0eb65aae5b33c734a22123d9e
Kevin A. Archie <karchie@alumni.caltech.edu>
2010-11-20 08:13:08 -0600

    Created get() to wrap urllib2 calls

--- arc-sync.py
+++ arc-sync.py
@@ -2,7 +2,8 @@
 import base64, getopt, json, os, StringIO as sio, sys, urllib2 as u, zipfile as z
 
 opts=dict(getopt.getopt(sys.argv[1:], 'h:u:p:m:l:', ['proj='])[0])
-auth={'Authorization':'Basic ' + base64.encodestring('%(-u)s:%(-p)s' % opts).strip()}
 
-for exp in filter(lambda x: x['label'] not in os.listdir(opts['-l']), json.loads(u.urlopen(u.Request(url='%(-h)s/REST/projects/%(--proj)s/experiments' % opts, headers=auth)).read())['ResultSet']['Result']) :
-        z.ZipFile(sio.StringIO(u.urlopen(u.Request(url=opts['-h'] + '%(URI)s/scans/ALL/files?format=zip' % exp, headers=auth)).read()), 'r').extractall(opts['-l'])
+def get(url): return u.urlopen(u.Request(url=opts['-h']+url, headers={'Authorization':'Basic '+base64.encodestring('%(-u)s:%(-p)s' % opts).strip()})).read()
+
+for expt in filter(lambda x: x['label'] not in os.listdir(opts['-l']), json.loads(get('/REST/projects/%(--proj)s/experiments' % opts))['ResultSet']['Result']) :
+        z.ZipFile(sio.StringIO(get('%(URI)s/scans/ALL/files?format=zip' % expt)), 'r').extractall(opts['-l'])
