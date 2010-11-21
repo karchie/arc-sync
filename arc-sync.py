7202204590c4f77097d450f8453cd9c6ff225fc6
Kevin A. Archie <karchie@alumni.caltech.edu>
2010-11-21 12:38:58 -0600

    argument to get is really a relative URI, not a URL

--- arc-sync.py
+++ arc-sync.py
@@ -3,6 +3,6 @@ import base64, getopt, json, os, StringIO as sio, sys, urllib2 as u, zipfile as 
 
 opts=dict(getopt.getopt(sys.argv[1:], 'h:u:p:m:l:', ['proj='])[0])
 
-def get(url): return u.urlopen(u.Request(url=opts['-h']+url, headers={'Authorization':'Basic '+base64.encodestring('%(-u)s:%(-p)s' % opts).strip()})).read()
+def get(uri): return u.urlopen(u.Request(url=opts['-h']+uri, headers={'Authorization':'Basic '+base64.encodestring('%(-u)s:%(-p)s' % opts).strip()})).read()
 
 map(lambda e: z.ZipFile(sio.StringIO(get('%(URI)s/scans/ALL/files?format=zip' % e)), 'r').extractall(opts['-l']), filter(lambda x: x['label'] not in os.listdir(opts['-l']), json.loads(get('/REST/projects/%(--proj)s/experiments' % opts))['ResultSet']['Result']))
